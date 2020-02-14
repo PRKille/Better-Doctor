63617bb740c9fdf84ae93c4e1177391d8710cb21
Patrick Kille <pr.kille@gmail.com>
2020-02-14 09:36:22 -0800

    create doctor service file and basic API call functions

---
+++ src/doctor-service.js
@@ -0,0 +1,32 @@
+export class DoctorService {
+  async findDoctorByAilment(ailment) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.5051%2C-122.6750%2C20&user_location=45.5051%2C-122.6750&fields=practices(name%2Caccepts_new_patients%2Cwebsite)%2Cvisit_address%2Cphones%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      const jsonresponse = await response.json();
+      return jsonresponse;
+    } catch(error) {
+      return error.message;
+    }
+     
+  }
+
+  async findDoctorByFirstName(firstName) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&location=45.5051%2C-122.6750%2C20&user_location=45.5051%2C-122.6750&fields=practices(name%2Caccepts_new_patients%2Cwebsite)%2Cvisit_address%2Cphones%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      const jsonresponse = await response.json();
+      return jsonresponse;
+    } catch(error) {
+      return error.message;
+    }
+  }
+
+  async findDoctorByLastName(lastName) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.5051%2C-122.6750%2C20&user_location=45.5051%2C-122.6750&fields=practices(name%2Caccepts_new_patients%2Cwebsite)%2Cvisit_address%2Cphones%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      const jsonresponse = await response.json();
+      return jsonresponse;
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
