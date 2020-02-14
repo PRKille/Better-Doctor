184a6d38f8d64c947ef1b2f6205492ebe689da28
Patrick Kille <pr.kille@gmail.com>
2020-02-14 10:13:51 -0800

    add refactored api call function

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -29,4 +29,14 @@ export class DoctorService {
       return error.message;
     }
   }
+
+  async findDoctor(first, last, ailment) {
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?`+first+last+ailment+`&location=45.5051%2C-122.6750%2C20&user_location=45.5051%2C-122.6750&fields=practices(name%2Caccepts_new_patients%2Cwebsite)%2Cvisit_address%2Cphones%2Cprofile&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      const jsonresponse = await response.json();
+      return jsonresponse;
+    } catch(error) {
+      return error.message;
+    }
+  }
 }
